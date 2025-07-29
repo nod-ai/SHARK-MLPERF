0491ef872f5396322df62747cee18ea468865b42
Ean Garvey <ean.garvey@amd.com>
2025-07-29 09:39:37 -0500

    Fixup accuracy venv setup script.

--- code/stable-diffusion-xl/SDXL_inference/setup_accuracy_env.sh
+++ code/stable-diffusion-xl/SDXL_inference/setup_accuracy_env.sh
@@ -5,7 +5,7 @@ set -e
 
 python3.11 -m venv accuracy_venv
 source accuracy_venv/bin/activate
-/mlperf/harness/accuracy_venv/bin/python -m pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/rocm6.1
-/mlperf/harness/accuracy_venv/bin/python -m pip install -r /mlperf/inference/text_to_image/requirements.txt
-/mlperf/harness/accuracy_venv/bin/python -m pip install pandas ijson numpy==1.26.4 # these are missing from the reqs
+python -m pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/rocm6.1
+python -m pip install -r /mlperf/inference/text_to_image/requirements.txt
+python -m pip install pandas ijson numpy==1.26.4 py-libnuma # these are missing from the reqs
 deactivate
